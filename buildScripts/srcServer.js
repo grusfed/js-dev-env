110a24aa4bc8ba84702193d309225e3c583105bb
grusfed <grusfed@gmail.com>
2016-11-22 20:12:13 +0200

    Finish JS development environment

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Bar", "email": "tammy@gmail.com"},
+    {"id": 31, "firstName": "Neviot", "lastName": "Maim", "email": "maim@gmail.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
